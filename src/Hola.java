9dd9b207fdf97006bcfdecc1596460fbd4ce43f7
aaalxi <aval0001@stud.ek.dk>
2025-10-22 14:23:10 +0200

    hiii

--- src/Hola.java
+++ src/Hola.java
@@ -1,6 +1,7 @@
 public class Hola {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Hola Bitch");
+        System.out.println("Hola ");
+
     }
 }
